cad65d1677d78d5186cf162e61322104cc427239
Ibrahim Beqiri <ibraa.cs@msn.com>
2017-12-12 11:28:59 +0100

    fix

--- src/test/java/com/book/test/PersistenceTest.java
+++ src/test/java/com/book/test/PersistenceTest.java
@@ -1,5 +1,11 @@
 package com.book.test;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Iterator;
+import java.util.List;
+
 import javax.persistence.EntityManager;
 
 import javax.persistence.EntityManagerFactory;
@@ -17,6 +23,58 @@ public class PersistenceTest {
 
 	@Test
 	public void test() {
+		try {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+			String cmd;
+			while (true) {
+				System.out
+						.println("Write a query [or 'populate' or 'quit']: ");
+				cmd = reader.readLine();
+
+				if ("populate".equals(cmd)) {
+					populate();
+				} else if ("quit".equals(cmd)) {
+					System.out.println("The End");
+					break;
+				} else {
+					executeRequest(cmd);
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void executeRequest(String cmd) {
+		List result = null;
+		EntityTransaction tx = null;
+		try {
+
+			
+			EntityManagerFactory emf = Persistence.createEntityManagerFactory("bookPU");
+			EntityManager em = emf.createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+			result = em.createQuery(cmd).getResultList();
+			Iterator it = result.iterator();
+			while (it.hasNext()) {
+				System.out.println(it.next());
+			}
+			tx.commit();
+
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+			try {
+				tx.rollback();
+			} catch (IllegalStateException e1) {
+				e1.printStackTrace();
+			} catch (SecurityException e1) {
+				e1.printStackTrace();
+			} 
+		}
+	}
+
+	public void populate() {
 		EntityTransaction tx = null;
 		try {
 
@@ -25,12 +83,25 @@ public class PersistenceTest {
 			tx = em.getTransaction();
 			tx.begin();
 
-			Category c = new Category("Fiction!");
+			Category c = new Category("Fiction");
 			Category c2 = new Category("Thriller");
 			Category c3 = new Category("Novel");
+			Category c4 = new Category("Drama");
+			Category c5 = new Category("Mystery");
+			Category c6 = new Category("Adventure");
+			Category c7 = new Category("Romance");
+			Category c8 = new Category("Fantasy");
 			
-			Writer w = new Writer("John", "Smith", "Sierre");
-			Writer w2 = new Writer("Jake", "Pratt", "NYC");
+			Writer w = new Writer("George", "Orwell", "Sierre");
+			Writer w1 = new Writer("Jake", "Pratt", "NYC");
+			Writer w2 = new Writer("John", "Smith", "Sierre");
+			Writer w3 = new Writer("Jake", "Pratt", "NYC");
+			Writer w4 = new Writer("John", "Smith", "Sierre");
+			Writer w5 = new Writer("Jake", "Pratt", "NYC");
+			Writer w6 = new Writer("John", "Smith", "Sierre");
+			Writer w7 = new Writer("Jake", "Pratt", "NYC");
+			Writer w8 = new Writer("John", "Smith", "Sierre");
+			Writer w9 = new Writer("Jake", "Pratt", "NYC");
 			
 			Book b = new Book("1 mile", "I-1234567890", "London House", "10-10-2016", 100, false, "Awesome");
 			Book b2 = new Book("test 2", "I-1234567", "idk", "10-10-2006", 100, true, "good");
